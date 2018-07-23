55be01403b744c1f032295a215dd373d2a8eb6ab
Morris Ravis <morrisravis@berkeley.edu>
2018-07-23 18:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function happyHolidays() {return "Happy holidays!"
 
 function name
 
-function happyHolidaysTo(name) {return string("Happy holidays, {$name}!")
+function happyHolidaysTo(name) {return string("Happy holidays, you!")
 }
